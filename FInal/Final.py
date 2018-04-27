57dd9127ff92b43d2ccf7a558fe493b4f80fced3
CarlosCordoba96 <carlos.cordoba@alu.uclm.es>
2018-04-27 21:43:13 +0200

    avanzando

---
+++ FInal/Final.py
@@ -0,0 +1,17 @@
+import bge
+def move(cont):
+    cube=cont.owner
+    leftPlane = bge.logic.getCurrentScene().objects['Plane.001'].worldPosition.x
+    rightPlane = bge.logic.getCurrentScene().objects['Plane.007'].worldPosition.x
+    pos=cube.worldPosition.x
+    print(pos)
+    prop=cube['move']
+    print(prop)
+    if(prop=="right"):
+        cube.applyMovement([0.1,0,0],False)
+        if(pos>=rightPlane):
+            cube['move']="left"
+    else:
+        cube.applyMovement([-0.1,0,0],False)
+        if(pos<=leftPlane):
+            cube['move']="right"
